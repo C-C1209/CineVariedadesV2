5e98859d01fc9ea5677808e73b202692389cf756
TinoCH8 <71781767+TinoCH8@users.noreply.github.com>
2021-06-28 23:14:34 -0600

    rutas
    rutas

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Pagina controlAdmin
+Route::group(['prefix'=>'admin','as'=>'admin.'], function(){
+   
+    Route::get('/', [App\Http\Controllers\Admin\AdminController::class,'index']);
+    Route::get('/peliculas', [App\Http\Controllers\Admin\PeliculasController::class,'index']);
+    Route::POST('/peliculas/edit', [App\Http\Controllers\Admin\PeliculasController::class,'edit']);
+    
+
 });
+
+    Route::get('/', [App\Http\Controllers\Cliente\ClienteController::class,'index']);
+    Route::get('/login', function () { return view('login'); });
+
+
+
+
+
+
+// Route::get('/controlAdmin', function () { return view('/controlAdmin/controlPanel'); });
+
+// //Ruta Peliculas del control Admin
+// Route::get('/controlAdmin/peliculas', function () { return view('/controlAdmin/peliculas'); });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
+
